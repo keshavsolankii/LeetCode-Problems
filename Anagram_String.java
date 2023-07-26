629ead75c1f5ceb93977e62bdc5dd31491b6cb8c
Keshav <110564015+keshavsolankii@users.noreply.github.com>
2023-07-27 02:58:51 +0530

    Create Anagram_String.java

---
+++ Anagram_String.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] charCount = new int[26];
+
+        // Count character frequencies in string s
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s.charAt(i) - 'a']++;
+        }
+
+        // Decrement character frequencies for string t
+        for (int i = 0; i < t.length(); i++) {
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+        // Check if all character frequencies are zero (anagrams)
+        for (int count : charCount) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
