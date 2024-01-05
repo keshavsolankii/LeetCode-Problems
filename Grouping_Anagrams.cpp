a39b5907ad65104e23ddeb2fd2a8fefe47bb25e1
Keshav <110564015+keshavsolankii@users.noreply.github.com>
2024-01-06 04:29:54 +0530

    Create Grouping_Anagrams.cpp

---
+++ Grouping_Anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> umap;
+
+        for(auto x : strs)
+        {
+            string word = x;
+            sort(word.begin(), word.end());
+            umap[word].push_back(x);
+        }
+        vector<vector<string>> ans;
+
+        for(auto x : umap)
+            ans.push_back(x.second);
+        
+        return ans;
+    }
+};
