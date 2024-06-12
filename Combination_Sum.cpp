f82b62ba52d2e7249ecb429f27febc0666cd4656
Keshav <110564015+keshavsolankii@users.noreply.github.com>
2024-06-13 04:48:02 +0530

    Create Combination_Sum.cpp

---
+++ Combination_Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void find(int ind,int target,vector<int>&candidates,vector<vector<int>>&ans,vector<int>&ds){
+        if(ind==candidates.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return ;
+        }
+        if(candidates[ind]<=target){
+            ds.push_back(candidates[ind]);
+            find(ind,target-candidates[ind],candidates,ans,ds);
+            ds.pop_back();
+        }
+        find(ind+1,target,candidates,ans,ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        find(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
